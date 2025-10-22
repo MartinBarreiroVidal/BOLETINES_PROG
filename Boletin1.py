b2788dc34b3210e99ba21cefc9f2c387300d0efe
Martín Barreiro <mbarreirovidal@danielcastelao.org>
2025-10-22 12:51:33 +0200

    Añadidos boletines 1 y 2

---
+++ Boletin1.py
@@ -0,0 +1,87 @@
+# 1. Escribir o resultado das seguintes expresións:
+print(((3+2) % 2 - 15) / 2 *5)
+print((6+(6) / 7) + 35 / 2 -8 * 5 / 4 * 2)
+print(3 + 6 * 14 % 3)
+print(8 + 7 * 3 + 4 * 6 /2 % 4)
+print(27 % 4 +15 / 4)
+print((7*3 - 4 * 4)*2 / 4 * 2)
+
+# 2. Cales dos seguintes nomes de variables non son válidos?
+
+cantidadeTotal= 3
+print(cantidadeTotal)
+
+
+#a)
+m = 3
+n = 2
+print((m+n) / n)
+
+#b)
+p = 5
+r = 3
+s = 2
+print((m+n/p) / ((p-r) /s))
+
+#c)
+q = 2
+print((m+4) / (p - q))
+
+#d)
+c = 2
+t = 4
+print((c * r * t) / (100))
+
+#e)
+print((m + n ) / (p + (q/r)))
+
+#f)
+print(m + n * (p + q))
+
+#g)
+i = 5
+print((n*((1+i)**t) * i)/(((1+i)**t)-1))
+
+#4. Avalia as seguintes expresións:
+
+ejem1 = True
+ejem2 = False
+print(ejem1 and ejem1)
+print(not ejem2 == ejem1)
+print((ejem1 and ejem1)) or ejem2
+print((ejem2 or ejem2)) and ejem2 != ejem1
+print((not(ejem1 and ejem2)))
+print("12" + "12")
+print("34" + "43")
+
+#5. Avaliar as seguintes espresións, tendo en conta que as variables  teñen os valores:
+i = 1
+j = 0
+k = 1
+print(i + k <=j - k * 3 and k >=2)<
+
+i = 3
+j = 2
+k = -1
+print(i == 3 or j <= 2 and k > 0)
+
+tipo = 10
+rede = 7.5
+print(tipo < rede + 1.5)
+
+ano = 1993
+print(ano % 400 ==0)
+
+print(3 == 2 or 5 > 1 + 1)
+
+print(5 - 2 > 4 and not(0.5 == 1 / 5))
+
+a = 2
+b = 5
+c = 6
+d = 10
+print(a >= b or a >= c and a <d)
+
+print(a + b < c and a + c < d or 2 * a < a + b)
+
+print(not (a * b < d) and not (a * b < c) or b + c <= d)
